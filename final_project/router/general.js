6ea212a583721c0aa476b155ccd6eabc1f8868ed
ryanphamit <ryanphamit@gmail.com>
2024-09-03 09:25:06 +0000

    Task 2 & 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,14 +17,23 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;
+    if (books[isbn]) {
+        return res.status(200).json(books[isbn]);
+    } else {
+        return res.status(404).json({ message: "Book not found." });
+    }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const authorBooks = Object.values(books).filter(book => book.author === req.params.author);
+  console.log(authorBooks)
+  if(authorBooks.length > 0) {
+    return res.status(200).json(authorBooks);
+  } else {
+    return res.status(404).json({ message: "Book not found." });
+  }
 });
 
 // Get all books based on title
